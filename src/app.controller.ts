c5484cecac9fb2b390cc156da032adc352676680
xKamaiL <xkamail@icloud.com>
2021-02-11 18:49:00 +0700

    update service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+  }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('rune')
+  getRuneFromOpGG(@Query('championName') championName: string): Promise<any> {
+    if (typeof championName === 'undefined') {
+      throw new BadRequestException('No Champion name');
+    }
+    return this.appService.fetchRuneConfig(championName);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,10 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  async searchForChampionName(){
+
+  }
+  async fetchRuneConfig(championName: string) {
+    return championName;
+  }
 }
